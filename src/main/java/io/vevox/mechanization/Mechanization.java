f27a0f087555057688ddafd088018dfcb0df9aa6
Matt (NukesForKids) <matt@capit.me>
2015-04-15 09:07:26 -0700

    Created project base.

---
+++ src/main/java/io/vevox/mechanization/Mechanization.java
@@ -0,0 +1,23 @@
+package io.vevox.mechanization;
+
+import io.vevox.vevoxel.api.VevoxelPlugin;
+
+/**
+ * @author Matthew Struble
+ */
+public class Mechanization extends VevoxelPlugin {
+    @Override
+    protected void loaded() {
+        // Nothing
+    }
+
+    @Override
+    protected void enabled() {
+
+    }
+
+    @Override
+    protected void disabled() {
+
+    }
+}
