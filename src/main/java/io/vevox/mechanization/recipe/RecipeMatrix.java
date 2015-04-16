6dc25ff9e35eaefc6270b7e0af11867f8783aa8b
Matt (NukesForKids) <matt@capit.me>
2015-04-16 13:39:39 -0700

    More work for matrix parser.

--- src/main/java/io/vevox/mechanization/recipe/RecipeMatrix.java
+++ src/main/java/io/vevox/mechanization/recipe/RecipeMatrix.java
@@ -1,17 +1,26 @@
 package io.vevox.mechanization.recipe;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Matthew Struble
  */
 public class RecipeMatrix implements Serializable {
 
+    private HashMap<Character, KeyParser> keys;
     private char[][] input,output;
 
     public RecipeMatrix(JsonObject matrix, int rows) throws NullPointerException, ClassCastException, IllegalStateException {
+        this.keys = new HashMap<>();
+        JsonObject keys = matrix.getAsJsonObject("keys");
+        for (Map.Entry<String,JsonElement> entry : keys.entrySet())
+            this.keys.put(entry.getKey().charAt(0), new KeyParser(entry.getValue().getAsJsonObject()));
+
 
     }
 
