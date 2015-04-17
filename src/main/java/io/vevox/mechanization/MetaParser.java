29270ec0e74f74beae8db5c7c5e764c1a9012879
Matthew Struble <matt@capit.me>
2015-04-16 19:08:12 -0700

    Added a colored name getter.

--- src/main/java/io/vevox/mechanization/MetaParser.java
+++ src/main/java/io/vevox/mechanization/MetaParser.java
@@ -1,6 +1,7 @@
 package io.vevox.mechanization;
 
 import com.google.gson.JsonObject;
+import org.bukkit.ChatColor;
 
 import java.io.Serializable;
 
@@ -14,6 +15,10 @@ public class MetaParser implements Serializable {
     public MetaParser(JsonObject meta) throws NullPointerException, ClassCastException, IllegalStateException {
         name = meta.get("name").getAsString();
         description = meta.get("description").getAsString();
+    }
+
+    public String coloredName(){
+        return ChatColor.translateAlternateColorCodes('&', name);
     }
 
 }
